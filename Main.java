f0163af5b296b661fc4d5baf1dc69c14f8d815cf
connorbettermann <39529970+connorbettermann@users.noreply.github.com>
2018-05-22 12:33:44 -0700

    src files

---
+++ Main.java
@@ -0,0 +1,29 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.scene.layout.StackPane;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.scene.layout.GridPane;
+import javafx.scene.control.TextField;
+
+import javax.swing.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception{
+
+        Application.launch(Login.class, args);
+
+
+    }
+}
